f5b50fca2df6fbd42ab446e1221201e9bec3caf8
vincent <quan229870530@email.com>
2016-01-14 15:19:46 +0800

    add a test class

---
+++ src/rog/itheima/googleplay10/test/test.java
@@ -0,0 +1,11 @@
+package rog.itheima.googleplay10.test;
+
+import android.util.Log;
+import android.widget.Toast;
+
+public class test {
+	public test() {
+		// TODO Auto-generated constructor stub
+		Log.e("TAG", "this is a test class!!!");
+	}
+}
